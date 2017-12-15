186ebb579df03f498e458637200dc6d5525115c9
Ram Radhakrishnan <ramharshvardhan@Rams-MacBook-Pro.local>
2017-12-14 20:39:06 -0800

    Initial Commit

---
+++ AlphaStocks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlphaStocks
+//
+//  Created by Ram Harshvardhan Radhakrishnan on 12/14/17.
+//  Copyright © 2017 Ram Harshvardhan Radhakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
